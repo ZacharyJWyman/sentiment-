ed5defbdf17d062affba7add1673986fbd8790da
ZacharyJWyman <zack.wyman1@gmail.com>
2020-09-25 00:45:19 -0700

    update transcription

--- Scripts/speech_to_text.py
+++ Scripts/speech_to_text.py
@@ -3,15 +3,24 @@
 
 import speech_recognition as sr
 import pyaudio
+import pandas as pd
+from helperFunctions import *
 
-jurors = ['nat', 'jon', 'dil', 'car']
+jurors = ['Zack', 'Ben']
 storage = []
+storage_df = pd.DataFrame()
+
 
 while len(storage) < len(jurors):
     print('Juror' + ' ' + jurors[len(storage)] + ' ' + 'is speaking:')
     init_rec = sr.Recognizer()
     with sr.Microphone() as source:
-        audio_data = init_rec.record(source, duration=5) #each juror speaks for 10 seconds
-        text = init_rec.recognize_google(audio_data)
+        audio_data = init_rec.adjust_for_ambient_noise(source)
+        audio_data = init_rec.listen(source) #each juror speaks for 10 seconds
+        audio_text = init_rec.recognize_google(audio_data)
         print('End of juror' + ' ' + jurors[len(storage)] + ' ' + 'speech')
-        storage.append(text)
+        storage.append(audio_text)
+        cleaned = clean_text(audio_text)
+        tokenized = tokenize_text(cleaned)
+        padded_text = padding(cleaned, tokenized) #fix padded text elongating rows
+        
