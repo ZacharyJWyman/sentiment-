a7617e965153c72b0dac613deacf27459adf5da1
Zachary Wyman <zack.wyman1@#gmail.com>
2020-09-28 22:30:08 -0700

    update

--- Scripts/helperFunctions.py
+++ Scripts/helperFunctions.py
@@ -1,6 +1,13 @@
 import re
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
+import pickle
+from keras.models import load_model
+
+#convert numpy array to sentiment 
+label_dict = {2: 'positive',
+           1: 'neutral',
+           0:'negative'}
 
 def clean_text(text, stem=False):
     text_clean = '@\S+|https?:\S|[^A-Za-z0-9]+'
@@ -9,14 +16,22 @@ def clean_text(text, stem=False):
     return text
 
 def tokenize_text(text):
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(text)
+    #load tokenizer obj
+    with open(r"C:\Users\19712\OneDrive\Desktop\sent_clone\sentiment-\model\tokenizer.pickle", 'rb') as handle:
+        tokenizer = pickle.load(handle)
     return tokenizer
 
 def padding(text, tokenizer):
-    text = pad_sequences(tokenizer.texts_to_sequences(text), 
-                       maxlen = 500)
+    text = pad_sequences(tokenizer.texts_to_sequences(text), maxlen = 500) #len is number of words
+    if isinstance(text, str):
+        text = [text]
     return text
 
 def apply_labels(label):
     return label_dict[label]
+
+def classify_sentiment(text):
+    score = loaded_model.predict(padded_text)
+    y_pred = np.argmax(score, axis = 1).reshape(-1,1)
+    y_sentiment = np.vectorize(label_dict.get)(y_pred)
+    return y_pred, y_sentiment
